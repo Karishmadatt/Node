3ad3d46145b7689a157bcd71b31c591dc44f26da
Karishmadatt <karishmadatt2000@gmail.com>
2023-03-17 18:36:29 +0530

    Master Promises 1

--- promises.js
+++ promises.js
@@ -1,15 +1,38 @@
-const check = false;
+const movies = require('./movies.json');
+const reviews = require('./reviews.json');
+const users = require('./users.json');
 
-const promise = new Promise((resolve,reject)=>{
-    if(check){
-        resolve('Done');
-    }else{
-        reject('Undone');
+const getMovie = (id)=>{
+    return new Promise((resolve,reject)=>{
+
+        setTimeout(() => {
+            const movie = movies.find((movie)=>movie.id===id);
+            if(movie){
+                resolve(movie);
+            }else{
+                reject('Not Found');
+            }
+        }, 1000);
+        });
     }
-})
 
-promise.then((res)=>{
-    console.log(res);
+const getreview = (id,cb)=>{
+    setTimeout(()=>{
+    const review = reviews.find((review)=>review.movie_id==id);
+    cb(review);
+    },1000);
+}
+
+const getuser = (name,cb)=>{
+    setTimeout(()=>{
+        const user = users.find((user)=>user.name===name);
+        cb(user)
+    },1000);
+}
+
+getMovie(1)
+.then(movie=>{
+    console.log(movie);
 })
 .catch(err=>{
     console.log(err);
